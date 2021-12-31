8fd4407cad3716b24744edf15d7883f10f02c7b8
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2021-12-31 11:18:05 +0900

    Python문제
    Python 기초 100제 - 95

--- CodeUP/basic100_95.py
+++ CodeUP/basic100_95.py
@@ -11,19 +11,22 @@ Python 기초 100제 - 95
 n개의 흰 돌이 놓인 위치를 출력하는 프로그램을 작성해보자.
 """
 
-#board = [[0 for j in range(20)] for i in range(20)] 이러한 리스트 생성 방식을 List Comprehensions라고 함.
+# board = [[0 for j in range(10)] for i in range(3)]
+# 0이 10개가 들어있는 리스트가 3개 만들어짐.
+# 이러한 리스트 생성 방식을 List Comprehensions라고 함.
+
 board = []
-for i in range(20):
+for i in range(20):     # 바둑판은 19 x 19이지만, 편의상 좌표를 1부터 시작할 것이기 때문에 20 x 20으로 만들어 줌.
     board.append([])
     for j in range(20):
         board[i].append(0)
 
-n = int(input())
+n = int(input())    # 몇 개의 바둑알을 놓은 것인가.
 for i in range(n):
     x, y = input().split()
     board[int(x)][int(y)] = 1
 
-for i in range(1, 20):
+for i in range(1, 20):  # 편의상 1부터 시작(20 x 20)
     for j in range(1, 20):
         print(board[i][j], end=' ')
     print()
