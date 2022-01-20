b6597798bc18ba5362f35d8e8217a98b30d57d59
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-01-20 14:02:20 +0900

    2차 시도 실패

--- BaekJoon/Greedy_1913.py
+++ BaekJoon/Greedy_1913.py
@@ -2,16 +2,39 @@
 한 개의 회의실이 있는데 이를 사용하고자 하는 N개의 회의에 대하여 회의실 사용표를 만들려고 한다. 각 회의 I에 대해 시작시간과 끝나는 시간이 주어져 있고, 각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 회의의 최대 개수를 찾아보자. 단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다. 회의의 시작시간과 끝나는 시간이 같을 수도 있다. 이 경우에는 시작하자마자 끝나는 것으로 생각하면 된다.
 '''
 
+from tracemalloc import start
+from turtle import end_fill
+
+
 room = []
-roomN = int(input())     # 회의 수를 roomN으로 입력
 
-for i in range(roomN):  # 회의 수 만큼 반복
+n = int(input())
+for i in range(n):
     room.append([])
-    startTime, endTime = map(int, input().split())  # 회의 시작 시간과 끝나는 시간을 입력 받는다.
-    room[i] = startTime, endTime    # 회의 사용표를 입력한다.
+    time = list(map(int, input().split()))  # 회의 시간을 입력받아 넣어준다.
+    room[i] = time
 
+room.sort() # 오름차순 정렬
 
-# print(room[0:roomN])
+count = 1   # 회의 개수
 
-count = 0
-# 회의 종료 시간이 제일 작아야 된다. 종료 시간이 같을 경우, 시작 시간이 작은 것부터 정렬한다. https://www.acmicpc.net/board/view/74752
+# 현재 회의 초기 값 정하기
+now = room[0]
+for i in room:
+    if now[1] > i[1]:
+        now = i
+
+# 다음 회의 중 종료 시간이 제일 빠른 회의 택(현재 회의에 넣음)
+# 다음 회의는 다음 회의 시작 시간이 현재 회의 종료 시간보다 크거나 같아야 됨
+
+for i in range(len(room)):
+    if now[1] <= room[i][0]:
+        now = room[i]
+        for j in room[i:]:
+            if now[1] > j[1]:
+                now = j
+        count += 1
+
+print(count)
+# 제출해서 확인해보기
+# -> 반례에서 틀림, 다시해보기
