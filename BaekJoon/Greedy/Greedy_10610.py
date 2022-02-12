91392de5af383bc5f1b465b187089c18c8c3e3a1
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-02-12 13:19:27 +0900

    성공(13/50) - 실버5

--- BaekJoon/Greedy/Greedy_10610.py
+++ BaekJoon/Greedy/Greedy_10610.py
@@ -4,28 +4,28 @@
 미르코를 도와 그가 만들고 싶어하는 수를 계산하는 프로그램을 작성하라.
 '''
 
-# 입력받은 숫자들로 만들 수 있는 숫자를 모두 만든다.
-# 만든 숫자 중 30의 배수이면서 제일 큰 수를 출력한다.
-# 30의 배수가 아니라면, -1을 출력한다.
+# 30 60 90 120 150 180 210 240 270 300
+# 마지막 숫자는 항상 0
+# 숫자들을 더하면 3의 배수가 나옴
+
+# 10의 5승까지 숫자를 입력할 수 있기 때문에 int로는 출력이 안 됨, 숫자를 출력할 때 문자열로 변경해야 됨.
 
 numbers = list(map(int, input()))
+# 내림차순으로 정렬
+numbers = sorted(numbers, reverse=True)
+number = []
 
-numbers.sort()  # 오름차순
+# 30의 배수인 경우
+if sum(numbers) % 3 == 0 and \
+numbers[-1] == 0:
+    for i in range(len(numbers)):
+        # 문자열로 바꾸기
+        number.append(str(numbers[i]))
 
-unit = 1    # 숫자 자릿수
-made_num = 0  # 만든 숫자
+    # 문자열 합치기
+    number = ''.join(number)    
+    print(number)   # 가장 큰 수 출력
 
-# 입력받은 숫자를 이용해서 숫자만들기
-for i in range(1, len(numbers)):
-    for num in numbers[i:]:
-        num *= unit
-        unit *= 10
-        made_num += num
-    
-    unit = 1
-    # if made_num % 30 == 0:
-    #     print(made_num)
-    #     break
-
-    # if len(numbers) == i+1:
-    #     print(-1)
+# 30의 배수가 아닌 경우
+else:
+    print(-1)
