12d4152b835c94d2ffb016a793b706d4db8769b6
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-02-25 12:29:21 +0900

    성공(18/50) - 골드4(수정 완료)

--- BaekJoon/Greedy/Greedy_1339.py
+++ BaekJoon/Greedy/Greedy_1339.py
@@ -1,62 +1,52 @@
 '''
-단어의 알파벳에게 숫자의 자릿수만큼 점수를 주고, 딕셔너리에 저장한다.
+단어 알파벳에게 숫자 자릿수에 해당하는 만큼 숫자를 더해준 후, 딕셔너리에 저장한다.
 ex) ABC -> A += 100, B += 10, C -> += 1
 
-알파벳 값들을 리스트로 따로 빼줘,
-점수가 큰 순서대로 숫자를 차례차례 곱해준다.
-
-점수들을 합하면 단어의 합 최댓값이 나온다.
+딕셔너리의 값을 내림차순으로 정렬 후,
+높은 값부터 낮은 값까지 순차적으로 숫자를 곱해준 값들을 더해줘 최댓값을 구한다.
 '''
 
-import string
 import sys
 
-# 알파벳 리스트
-alphabet = list(string.ascii_uppercase)
+word_n = int(input())   # 단어 개수
 
-# 알파벳 초기값
-zero = []
-for i in range(26):
-    zero.append(0)
+alphabets = {}      # 알파벳 딕셔너리
+alphabet_n = []     # 알파벳 점수 리스트
+words = []          # 단어 리스트
+num = 10            # 자릿수
+array_n = 1         # 배열 번호
+n_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]      # 숫자 리스트
 
-# 각 알파벳과 값을 넣어놓은 딕셔너리
-alphabets = dict(zip(alphabet, zero))
-
-# 입력받을 단어 개수 입력
-n = int(input())
-
-words = []   # 단어 리스트
 # 단어 입력받기
-for i in range(n):
-    words.append([])
+for i in range(word_n):
     word = list(sys.stdin.readline().strip())
-    # 알파벳에 점수를 주기위해 단어를 뒤집음
-    word = word[::-1]
-    words[i] = word
+    words.append(word)
 
-score = 1     # 숫자 자릿수만큼 점수주기
-# 알파벳에 점수주기
-for i in range(n):
-    for j in range(len(words[i])):
-        alphabets[words[i][j]] += score
-        score *= 10
-    score = 1
+# 단어 알파벳에게 자릿수에 해당하는 만큼 숫자 더해주기
+for i in range(word_n):
+    for a in words[i]:
+        if a in alphabets:
+            alphabets[a] += num ** (len(words[i]) - array_n)
+            array_n += 1        # 배열 번호 증가
+        else:
+            alphabets[a] = num ** (len(words[i]) - array_n)
+            array_n += 1        # 배열 번호 증가
+    array_n = 1
 
-score_list = []     # 알파벳 점수 리스트
-# 알파벳 점수를 내림차순으로 정렬해준다.
+# 딕셔너리 값들을 모아준다.
 for val in alphabets.values():
-    score_list.append(val)
+    alphabet_n.append(val)
 
-score_list = sorted(score_list, reverse=True)
+alphabet_n = sorted(alphabet_n, reverse=True)
 
-n = 9           # 숫자
-result = 0      # 결과
+# 숫자 곱해주기
+array_n = 0
 
-# 알파벳 점수 리스트 첫번째가 제일 점수가 큰 알파벳 정보이므로 숫자9부터 차례차례 곱해준다.
-for s in score_list:
-    s *= n
-    result += s
-    n -= 1
+for n in n_list:
+    if array_n == len(alphabet_n):
+        break
+    alphabet_n[array_n] *= n
+    array_n += 1
 
-# 결과 출력
-print(result)
+# n개의 단어 합 최댓값 출력
+print(sum(alphabet_n))
