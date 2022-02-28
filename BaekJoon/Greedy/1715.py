cc6288009e564952421a0cdbabc85f522a74c704
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-02-28 12:57:22 +0900

    2차 시도 실패

--- BaekJoon/Greedy/1715.py
+++ BaekJoon/Greedy/1715.py
@@ -1,14 +1,16 @@
 '''
 카드 묶음의 합이 더 적은 두 묶음끼리 합치면 최솟값이 나온다
+
+1. 카드 묶음의 합이 더 적은 두 묶음끼리 합쳐준다.
+2. 합쳐준 묶음들의 합을 결과값에 더해주고, 기존에 있던 카드 묶음의 리스트를 초기화하고 합쳐준 묶음들을 넣어준다.
+3. 카드 묶음의 개수가 1개가 될때까지 1, 2번을 반복한다.
+4. 카드 묶음의 개수가 1이 되면 카드 묶음의 합과 결과값을 더해서 출력한다.
 '''
 
 import sys
 
 cards = []              # 카드 묶음 리스트
-sum = 0                 # a와 b 카드 묶음 합
-sum_list = []           # 카드 묶음 합 리스트
-array_n = 2             # 배열 번호
-result = 0              # 카드 묶음 합 결과
+result = 0              # 결과값
 
 n = int(input())        # n개의 카드 묶음
 
@@ -19,28 +21,27 @@ for i in range(n):
 
 cards = sorted(cards)       # 카트 묶음 개수를 오름차순으로 정렬
 
-# 카드 묶음 더해주기
-sum = cards[0] + cards[1]
-sum_list.append(sum)
 while True:
-    if array_n >= len(cards):       # 배열 번호가 카드 묶음 개수보다 클 경우
+    # 카드 묶음 개수가 1개라면 비교가 필요가 없다.
+    if len(cards) == 1:
         break
 
-    if len(cards) == 3:             # 카드 묶음 개수가 3개일 경우는 비교 필요 없이 합해주면 됨
-        sum += cards[array_n]
-        sum_list.append(sum)
-        break
+    # # 카드 묶음 개수가 2개인 경우, 생각!!!!!
+    # elif len(cards) == 2:
+    #     pass
     
-    next = cards[array_n] + cards[array_n+1]    # 두번째 카드 묶음의 합
-    sum += cards[array_n]                       # 첫번째 카드 묶음의 합
-    if sum > next:
-        sum_list.append(next)
-        array_n += 2
-    else:
-        sum_list.append(sum)
-        array_n += 1
+    # # 카드 묶음 개수가 3개인 경우, 생각!!!!!
+    # elif len(cards) == 3:
+    #     pass
 
-# 카드 묶음들의 합 최솟값 출력
-for n in sum_list:
-    result += n
+    '''
+    1, 2번 만들어야 됨
+    '''
+    # 반복문이 돌아가고 카드 묶음 개수가 1개라면 비교가 다 끝난 상태다.
+    if len(cards) == 1:
+        result += cards[0]
+        break
+
+
+# 최솟값 출력
 print(result)
