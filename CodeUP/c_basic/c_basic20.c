3ba9185686f374f4578b8143154e17748d627208
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-03-24 16:28:15 +0900

    c언어 기초 100제(1~22)

---
+++ CodeUP/c_basic/c_basic20.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int front, back;
+    scanf("%d-%d", &front, &back);
+    printf("%06d%07d", front, back);
+
+    return 0;
+}
---
+++ CodeUP/c_basic/c_basic21.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char data[51];
+    scanf("%s", data);
+    printf(data);
+
+    return 0;
+}
